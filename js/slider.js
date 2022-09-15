f5de54c70611400050e934bc0f06166b305672ac
DabasishDasJoy <dabasishdasjoy92@gmail.com>
2022-09-15 20:35:22 +0600

    make changing of images

---
+++ js/slider.js
@@ -0,0 +1,18 @@
+const images = [
+    'images/pic-1.jpg', 
+    'images/pic-2.jpg', 
+    'images/pic-3.jpg', 
+    'images/pic-4.jpg', 
+    'images/pic-5.jpg', 
+    'images/pic-6.jpg', 
+    'images/pic-7.jpg', 
+    'images/pic-8.jpg', 
+    'images/pic-9.jpg' 
+]
+
+let imageIndex = 0;
+setInterval(() =>{
+    const slider = document.getElementById('slider');
+    slider.setAttribute('src', images[imageIndex % images.length])
+    imageIndex++;
+}, 2000);
